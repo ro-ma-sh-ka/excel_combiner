db22f77c8637741a62324ca8f25bbecdda035c84
romashka <roma.l@mail.ru>
2022-12-31 22:21:49 +0400

    looks good with pandas

--- by_pandas.py
+++ by_pandas.py
@@ -1,42 +1,31 @@
 import os
 import pandas as pd
-from openpyxl import load_workbook
 
 dir_name = './выгрузка озон'
 files = os.listdir(dir_name)
 pattern_sheet = 'Шаблон для поставщика'
 
-for file in files:
+
+def read_excel(dir_name, file, pattern_sheet):
     excel_file = os.path.join(dir_name, file)
     workbook = pd.read_excel(excel_file, sheet_name=pattern_sheet, skiprows=[0, 2])
-    print(workbook)
-    print()
-    print(workbook.columns.ravel())
-    #
-    # worksheet = workbook[pattern_sheet]
-    # columns_list = []
-    # current_row = 2
-    # for col in worksheet.iter_cols(min_row=2):
-    #     if col[0].value not in columns_list:
-    #         columns_list.append(col[0].value)
-    #         new_column = []
-    #         for row in col:
-    #             new_column.append(row.value)
-    #         df[col[0].value] = new_column
-    #
-    # writer = pd.ExcelWriter('test.xlsx')
-    # df.to_excel(writer)
-    # writer.save()
+    return workbook
 
-# https://dfedorov.spb.ru/pandas/%D0%A2%D0%B8%D0%BF%D0%B8%D1%87%D0%BD%D1%8B%D0%B5%20%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B8%20Excel,%20%D0%BF%D1%80%D0%BE%D0%B4%D0%B5%D0%BC%D0%BE%D0%BD%D1%81%D1%82%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%20%D0%B2%20pandas.html
-# df_base = pd.DataFrame()
+
+combiner = pd.DataFrame()
+for file in files:
+    wb = read_excel(dir_name, file, pattern_sheet)
+    combiner = pd.concat([combiner, wb], sort=False, axis=0)
+
+# file1 = "2022-12-27 (Don't use) Аксессуары для клюшки.xlsx"
+# wb1 = read_excel(dir_name, file1, pattern_sheet)
+# print(wb1)
+# file2 = "2022-12-27 SUP-доска.xlsx"
+# wb2 = read_excel(dir_name, file2, pattern_sheet)
+# print(wb2)
 #
-# for file in files:
-#     excel_file = os.path.join(dir_name, file)
-#     df = pd.read_excel(excel_file, sheet_name=pattern_sheet)
-#     df_base = df_base.reindex(columns=df.columns)
-#     df_base = df_base.append(df, ignore_index=True)
-#
-#     writer = pd.ExcelWriter('test.xlsx')
-#     df_base.to_excel(writer)
-#     writer.save()
+# combiner = pd.concat([wb1, wb2], sort=False, axis=0)
+
+writer = pd.ExcelWriter('test.xlsx')
+combiner.to_excel(writer)
+writer.save()
